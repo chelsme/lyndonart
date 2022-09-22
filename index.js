6b8450db850f3262e7353d4276169d710f129152
Chelsea Crowson <chelsea.crowson@me.com>
2022-09-21 22:24:15 -0500

    add new images

---
+++ index.js
@@ -0,0 +1,51 @@
+window.onload = () => {
+  loadPaintings()
+  loadCGI()
+  loadDrawings()
+  loadIllustrations()
+}
+
+let paintings = ["abstract-1.jpg", "abstract-2.jpg", "basketball.jpg", "butterfly-large.jpg", "butterfly-mural.jpg", "chelsea.jpg", "creation-of-planets.jpg", "flying-woman.jpg", "hippies.jpg", "island-heads.jpg", "mermaid-large.jpg", "moon.jpg", "mythical-music.jpg", "narcissist-large.jpg", "ocean.jpg", "red-circles.jpg", "roller-skater.jpg", "selfportrait-large.jpg", "sheilaportrait-large.jpg", "uncle-sam.jpg", "uninvited-large.jpg", "victor-colorado.jpg", "windy-woman.jpg", "wresting_with_demons.jpg"]
+let cgi = ["airco.jpg", "circles.jpg", "clubcameron-large.jpg", "drivers-large.jpg", "epv-peeps-large.jpg", "hotguitar-large.jpg", "hrolympiciconsfin-large.jpg", "mosquito.jpg", "my-peeps-large.jpg", "package-arctic.jpg", "package-desert.jpg", "storminnormans-large.jpg", "tune-wrangler.jpg", "valentine-large.jpg"]
+let drawings = ["animals.jpg", "another-card.jpg", "archetypes.jpg", "building.jpg", "horns.jpg", "playing-card.jpg", "robot.jpg", "santa.jpg"]
+let illustrations = ["a-boy-and-his-crab.jpg", "carousel.jpg", "cowboy.jpg", "highways-of-life.jpg", "hng.jpg", "houston-rodeo.jpg", "jeepstakes.jpg", "music.jpg", "unique-corner.jpg"]
+
+const loadPaintings = () => {
+  var src = document.getElementById("painting");
+  paintings.forEach(image => {
+    var img = document.createElement("img");
+    img.src = `images/paintings/${image}`;
+    img.className = "galleryimg";
+    src.appendChild(img);
+  });
+}
+
+const loadCGI = () => {
+  var src = document.getElementById("cgi");
+  cgi.forEach(image => {
+    var img = document.createElement("img");
+    img.src = `images/cgi/${image}`;
+    img.className = "galleryimg";
+    src.appendChild(img);
+  });
+}
+
+const loadDrawings = () => {
+  var src = document.getElementById("drawings");
+  drawings.forEach(image => {
+    var img = document.createElement("img");
+    img.src = `images/drawings/${image}`;
+    img.className = "galleryimg";
+    src.appendChild(img);
+  });
+}
+
+const loadIllustrations = () => {
+  var src = document.getElementById("illustrations");
+  illustrations.forEach(image => {
+    var img = document.createElement("img");
+    img.src = `images/illustrations/${image}`;
+    img.className = "galleryimg";
+    src.appendChild(img);
+  });
+}
