82da791f37f3e8a8df70d7065d9a5a9219e5349d
Chelsea Crowson <chelsea.crowson@me.com>
2022-09-21 23:44:44 -0500

    click listeners

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ window.onload = () => {
   loadIllustrations()
 }
 
+window.addEventListener("DOMContentLoaded", function () {
+  let closeButton = document.getElementById("close");
+  closeButton.addEventListener("click", closeModal);
+});
+
 let paintings = ["abstract-1.jpg", "abstract-2.jpg", "basketball.jpg", "butterfly-large.jpg", "butterfly-mural.jpg", "chelsea.jpg", "creation-of-planets.jpg", "flying-woman.jpg", "hippies.jpg", "island-heads.jpg", "mermaid-large.jpg", "moon.jpg", "mythical-music.jpg", "narcissist-large.jpg", "ocean.jpg", "red-circles.jpg", "roller-skater.jpg", "selfportrait-large.jpg", "sheilaportrait-large.jpg", "uncle-sam.jpg", "uninvited-large.jpg", "victor-colorado.jpg", "windy-woman.jpg", "wresting_with_demons.jpg"]
 let cgi = ["airco.jpg", "circles.jpg", "clubcameron-large.jpg", "drivers-large.jpg", "epv-peeps-large.jpg", "hotguitar-large.jpg", "hrolympiciconsfin-large.jpg", "mosquito.jpg", "my-peeps-large.jpg", "package-arctic.jpg", "package-desert.jpg", "storminnormans-large.jpg", "tune-wrangler.jpg", "valentine-large.jpg"]
 let drawings = ["animals.jpg", "another-card.jpg", "archetypes.jpg", "building.jpg", "horns.jpg", "playing-card.jpg", "robot.jpg", "santa.jpg"]
@@ -16,6 +21,7 @@ const loadPaintings = () => {
     var img = document.createElement("img");
     img.src = `images/paintings/${image}`;
     img.className = "galleryimg";
+    img.addEventListener("click", () => clickOnImage(`images/paintings/${image}`));
     src.appendChild(img);
   });
 }
@@ -26,6 +32,7 @@ const loadCGI = () => {
     var img = document.createElement("img");
     img.src = `images/cgi/${image}`;
     img.className = "galleryimg";
+    img.addEventListener("click", () => clickOnImage(`images/cgi/${image}`));
     src.appendChild(img);
   });
 }
@@ -36,6 +43,7 @@ const loadDrawings = () => {
     var img = document.createElement("img");
     img.src = `images/drawings/${image}`;
     img.className = "galleryimg";
+    img.addEventListener("click", () => clickOnImage(`images/drawings/${image}`));
     src.appendChild(img);
   });
 }
@@ -46,6 +54,22 @@ const loadIllustrations = () => {
     var img = document.createElement("img");
     img.src = `images/illustrations/${image}`;
     img.className = "galleryimg";
+    img.addEventListener("click", () => clickOnImage(`images/illustrations/${image}`));
     src.appendChild(img);
   });
+}
+
+const clickOnImage = (image) => {
+  let modal = document.getElementById("modal");
+  var img = document.createElement("img");
+  img.src = image;
+  img.classList.add("modal-img");
+  modal.appendChild(img);
+  modal.style.display = 'flex';
+}
+
+const closeModal = () => {
+  let modal = document.getElementById("modal");
+  modal.style.display = 'none';
+  modal.removeChild(modal.lastChild)
 }
