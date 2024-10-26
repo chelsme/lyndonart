d379649ca323f09e478b4c1c8b801c9e79472633
chelsea <chelsea.crowson@me.com>
2024-10-26 17:51:11 -0500

    cybergenic

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ window.addEventListener("DOMContentLoaded", function () {
 });
 
 let paintings = ["abstract-1.jpg", "abstract-2.jpg", "basketball.jpg", "butterfly-large.jpg", "butterfly-mural.jpg", "chelsea.jpg", "creation-of-planets.jpg", "flying-woman.jpg", "hippies.jpg", "island-heads.jpg", "mermaid-large.jpg", "moon.jpg", "mythical-music.jpg", "narcissist-large.jpg", "ocean.jpg", "red-circles.jpg", "roller-skater.jpg", "selfportrait-large.jpg", "sheilaportrait-large.jpg", "uncle-sam.jpg", "uninvited-large.jpg", "victor-colorado.jpg", "windy-woman.jpg", "wresting_with_demons.jpg"]
-let cgi = ["airco.jpg", "circles.jpg", "clubcameron-large.jpg", "drivers-large.jpg", "epv-peeps-large.jpg", "hotguitar-large.jpg", "hrolympiciconsfin-large.jpg", "mosquito.jpg", "my-peeps-large.jpg", "package-arctic.jpg", "package-desert.jpg", "storminnormans-large.jpg", "tune-wrangler.jpg", "valentine-large.jpg"]
+let cgi = ["airco.jpg", "circles.jpg", "clubcameron-large.jpg", "cybergenic.jpg", "drivers-large.jpg", "epv-peeps-large.jpg", "hotguitar-large.jpg", "hrolympiciconsfin-large.jpg", "mosquito.jpg", "my-peeps-large.jpg", "package-arctic.jpg", "package-desert.jpg", "storminnormans-large.jpg", "tune-wrangler.jpg", "valentine-large.jpg"]
 let drawings = ["animals.jpg", "another-card.jpg", "archetypes.jpg", "building.jpg", "horns.jpg", "playing-card.jpg", "robot.jpg", "santa.jpg"]
 let illustrations = ["a-boy-and-his-crab.jpg", "carousel.jpg", "cowboy.jpg", "highways-of-life.jpg", "hng.jpg", "houston-rodeo.jpg", "jeepstakes.jpg", "music.jpg", "unique-corner.jpg"]
 
