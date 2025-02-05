45023a0671105298d3df5ba3d6cddf683451d613
chelsea <chelsea.crowson@me.com>
2025-02-05 08:46:16 -0600

    art updates

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ window.addEventListener("DOMContentLoaded", function () {
   closeButton.addEventListener("click", closeModal);
 });
 
-let paintings = ["abstract-1.jpg", "abstract-2.jpg", "indian-pilgrim.jpg", "butterfly-large.jpg", "butterfly-mural.jpg", "chelsea.jpg", "creation-of-planets.jpg", "hippies.jpg", "island-heads.jpg", "mermaid-large.jpg", "moon-party.jpg", "mythical-music.jpg", "narcissist-large.jpg", "numbers.jpg", "red-circles.jpg", "robot-color.jpg", "roller-skater.jpg", "self-portrait.jpg", "sheila-portrait.jpg", "sight.jpg", "sirens.jpg", "super-girl.jpg", "uncle-sam.jpg", "uninvited-large.jpg", "victor-colorado.jpg", "windy-woman.jpg", "wresting_with_demons.jpg"]
-let cgi = ["airco.jpg", "circles.jpg", "clubcameron-large.jpg", "cybergenic.jpg", "drivers-large.jpg", "epv-peeps-large.jpg", "hotguitar-large.jpg", "hrolympiciconsfin-large.jpg", "mosquito.jpg", "my-peeps-large.jpg", "package-arctic.jpg", "desert-package.jpg", "storminnormans-large.jpg", "superman.jpg", "tune-wrangler.jpg", "valentine-large.jpg"]
-let drawings = ["animals.jpg", "another-card.jpg", "archetypes.jpg", "building.jpg", "horns.jpg", "playing-card.jpg", "robot.jpg", "santa.jpg"]
-let illustrations = ["a-boy-and-his-crab.jpg", "carousel.jpg", "cowboy.jpg", "highways-of-life.jpg", "hng.jpg", "house.jpg", "houston-rodeo.jpg", "jeepstakes.jpg", "majic.jpg", "unique-corner.jpg"]
+let paintings = ["abstract-1.jpg", "abstract-2.jpg", "indian-pilgrim.jpg", "butterfly.jpg", "candlelight_mural.jpg", "chelsea.jpg", "creation-of-planets.jpg", "hippies.jpg", "island-heads.jpg", "lady_w_guitar.jpg", "mermaid-large.jpg", "moon-party.jpg", "narcissist-large.jpg", "numbers.jpg", "red-circles.jpg", "robot-color.jpg", "skater.jpg", "self-portrait.jpg", "sheila-portrait.jpg", "sight.jpg", "sirens.jpg", "super-girl.jpg", "uncle-sam.jpg", "uninvited.jpg", "victor-colorado.jpg", "windy-woman.jpg", "wrestling_w_demons.jpg"]
+let cgi = ["airco.jpg", "circles.jpg", "clubcameron-large.jpg", "cybergenic.jpg", "drivers-large.jpg", "epv-peeps-large.jpg", "gardener.jpg", "hotguitar-large.jpg", "hr_olympics.jpg", "hrolympiciconsfin-large.jpg", "mosquito.jpg", "my-peeps-large.jpg", "arctic-package.jpg", "desert-package.jpg", "storminnormans-large.jpg", "superman.jpg", "tune-wrangler.jpg", "unique_corner.jpg", "valentine-large.jpg", "5_icons_OTC.jpg", "invite_OTC_cover.jpg", "invite_OTC_outside.jpg", "invite_OTC_inside.jpg"]
+let drawings = ["animals.jpg", "another-card.jpg", "archetypes.jpg", "building.jpg", "horns.jpg", "playing-card.jpg", "rapture.jpg", "robot.jpg", "santa.jpg"]
+let illustrations = ["a-boy-and-his-crab.jpg", "carousel.jpg", "cowboy.jpg", "cowboy_santa.jpg", "dreamhouse.jpg", "highways-of-life.jpg", "hng.jpg", "house.jpg", "houston-rodeo.jpg", "jeepstakes.jpg", "majic.jpg"]
 
 const loadPaintings = () => {
   var src = document.getElementById("painting");
